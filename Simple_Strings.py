e7bb486d436c4be5bcc0cfed03f356777ca943cb
Vip Patel <jvrapatel@gmail.com>
2020-08-30 11:16:33 -0400

    Initial commit

---
+++ Simple_Strings.py
@@ -0,0 +1,17 @@
+print ("SIMPLE STRING:")
+print ("Open Source Attorney")
+
+print ("NEXT LINE IN STRING:")
+print ("-Vip Patel\nOpen Source Attorney")
+
+print ("QUOTATION IN STRING: use for any litteral word or symbol")
+print ("it is almost never \"literal\" when someone says litteraly")
+print ("\n")
+
+print ("STRING VARIABLE:")
+phrase = "Open Source Attorney"
+print (phrase)
+print ("\n")
+
+print ("CONCATANATION: appending one string to another")
+print (phrase + " at Google")
